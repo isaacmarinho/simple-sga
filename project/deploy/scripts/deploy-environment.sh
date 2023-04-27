0907c7a57bad179f115632a1adeeafd5a3021c94
isaacmarinho <isaacmarinho@gmail.com>
2023-04-27 02:21:25 -0300

    Updating deployment script
    - Adding frontend deployment

--- project/deploy/scripts/deploy-environment.sh
+++ project/deploy/scripts/deploy-environment.sh
@@ -3,6 +3,8 @@
 export AWS_REGISTRY_URI="${AWS_ID}.dkr.ecr.us-east-1.amazonaws.com"
 export AWS_REGISTRY_ENVIRONMENTAL_IMAGE="${AWS_REGISTRY_URI}/simple-sga-environmental:latest"
 export AWS_REGISTRY_APP_IMAGE="${AWS_REGISTRY_URI}/sga-app:latest"
+export PROJECT_UI_FOLDER="../../../environmental/dist/sga-app"
+export CLOUD_FRONT_PROJECT_ID="<CLOUD_FRONT_PROJECT_ID>"
 
 # environmental
 aws ecr get-login-password --region us-east-1 --profile simple-sga | docker login --username AWS --password-stdin $AWS_REGISTRY_URI
@@ -26,3 +28,9 @@ docker tag sga-app:latest $AWS_REGISTRY_APP_IMAGE
 docker push $AWS_REGISTRY_APP_IMAGE
 
 aws ecs update-service --profile simple-sga --cluster simple-sga-cluster --service simple-sga-app-service --task-definition simple-sga-app-task --region us-east-1 --force-new-deployment
+
+aws s3 sync --profile simple-sga "$PROJECT_UI_FOLDER" s3://simple-sga-app/  --delete --exclude "*.svg"
+
+aws s3 cp --profile simple-sga "$PROJECT_UI_FOLDER" s3://simple-sga-app/ --recursive --exclude "*" --include "*.svg" --content-type "image/svg+xml"
+
+aws cloudfront create-invalidation --profile simple-sga --distribution-id $CLOUD_FRONT_PROJECT_ID --paths '/*'

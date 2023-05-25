50b5c25091ac808929ac981b5923261b62719fcb
isaacmarinho <isaacmarinho@gmail.com>
2023-05-25 11:44:30 -0300

    Updating sga-app deployment script

--- project/deploy/scripts/deploy-sga-app.sh
+++ project/deploy/scripts/deploy-sga-app.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env /bin/bash
 
-export PROJECT_UI_FOLDER="../../../environmental/dist/sga-app"
-export CLOUD_FRONT_PROJECT_ID="<CLOUD_FRONT_PROJECT_ID>"
+export PROJECT_UI_FOLDER="sga-app/dist/sga-app"
+export CLOUD_FRONT_PROJECT_ID="${CLOUD_FRONT_PROJECT_ID}"
+
+cd sga-app
+
+npm install ci && npm run build
+
+cd ../
 
 # sga-app
 aws s3 sync --profile simple-sga "$PROJECT_UI_FOLDER" s3://simple-sga-app/  --delete --exclude "*.svg"

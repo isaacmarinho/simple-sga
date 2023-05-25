abd29ad9162cfe3315bb6f92b3f5b14d31cc88d2
isaacmarinho <isaacmarinho@gmail.com>
2023-05-25 11:45:06 -0300

    Creating attachment deployment script

--- project/deploy/scripts/deploy-attachment.sh
+++ project/deploy/scripts/deploy-attachment.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env /bin/bash
 
 export AWS_REGISTRY_URI="${AWS_ID}.dkr.ecr.us-east-1.amazonaws.com"
-export AWS_REGISTRY_ENVIRONMENTAL_IMAGE="${AWS_REGISTRY_URI}/simple-sga-environmental:latest"
+export AWS_REGISTRY_ATTACHMENT_IMAGE="${AWS_REGISTRY_URI}/simple-sga-attachment:latest"
 
-# environmental
+# attachment
 aws ecr get-login-password --region us-east-1 --profile simple-sga | docker login --username AWS --password-stdin "$AWS_REGISTRY_URI"
 
-docker build -t simple-sga-environmental:latest -f environmental/deploy/Dockerfile .
+docker build -t simple-sga-attachment:latest -f attachment/deploy/Dockerfile .
 
-docker tag simple-sga-environmental:latest "$AWS_REGISTRY_ENVIRONMENTAL_IMAGE"
+docker tag simple-sga-attachment:latest "$AWS_REGISTRY_ATTACHMENT_IMAGE"
 
-docker push "$AWS_REGISTRY_ENVIRONMENTAL_IMAGE"
+docker push "$AWS_REGISTRY_ATTACHMENT_IMAGE"
 
-aws ecs update-service --profile simple-sga --cluster simple-sga-cluster --service simple-sga-environmental-service --task-definition simple-sga-environmental-task --region us-east-1 --force-new-deployment
+aws ecs update-service --profile simple-sga --cluster simple-sga-cluster --service simple-sga-attachment-service --task-definition simple-sga-attachment-task --region us-east-1 --force-new-deployment
